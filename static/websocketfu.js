8768b5629f4193f3c6e91e72f738ed333ac1f191
James Moughan <jamougha@gmail.com>
2014-03-03 20:08:19 +0000

    It's alive! It's alivegit status

--- static/websocketfu.js
+++ static/websocketfu.js
@@ -3,13 +3,15 @@
   DEBUG = true;
   google.load('visualization', '1.0', {'packages': ['corechart']});
 
-  var months = {};
-  var m = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 
-           'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-  for (var i = 0; i < m.length; i++) {
-    months[m[i]] = i;
-  }
+  var MONTHS = {
+    'JAN': 0, 'FEB': 1, 'MAR': 2,
+    'APR': 3, 'MAY': 4, 'JUN': 5, 
+    'JUL': 6, 'AUG': 7, 'SEP': 8, 
+    'OCT': 9, 'NOV': 11, 'DEC': 12
+  };
 
+  // ordering of matching date strings in three formats:
+  // `1999`, `1999 Q3`,  `1999 FEB`
   function cmpPeriods(t1, t2) {
     var y1 = parseInt(t1), y2 = parseInt(t2);
     if (y1 !== y2) {
@@ -29,19 +31,23 @@
     // monthly data - format '1997 JAN'
     var m1 = t1.substring(5, 8), 
         m2 = t1.substring(5, 8);
-    return months[m1] - months[m2];
+    return MONTHS[m1] - MONTHS[m2];
   }
 
 
   function Chart() {
     this.scale = 'linear';
     this.period = 'yearly';
-    this.periods = {
+
+    // indexes the JSON array of data returned when the user 
+    // clicks on a cdid
+    this.PERIODS = {
       yearly: 0,
       quarterly: 1,
       monthly: 2
     };
 
+    // options for the google chart api
     this.options = {
       'title': '',
       'width': 800,
@@ -50,11 +56,28 @@
         logScale: false
       }
     };
+
+    // The colours of the lines on the chart. If we 
+    // run out use black.
+    this.colours = [
+      '#3366cc', '#dc3912', '#ff9900',
+      '#109618', '#990099', '#0099c6',
+      '#dd4477', '#66aa00', '#b82e2e'
+    ];
+    this.colours.reverse();
+    // Keep a reference copy of the order of the
+    // colours so that the order they're used in
+    // doesn't change 
+    this.COLOURS = this.colours.slice(0);
+    console.log(this.COLOURS);
+
+    // The data to be plotted, and also its id and name
+    this.chosen = [];
   }
 
   Chart.prototype.draw = function () {
-    var chosen = $('#chosen').children(), 
-        titles = ['Year'],
+    //TODO: refactor with less spaghetti
+    var titles = ['Year'],
         dateMaps = [], // mappings from date to value for each plot
         allDates = {},
         dateList = [],
@@ -62,11 +85,16 @@
         table = [titles],
         i, j, datecolumn, date, value, row, chart;
 
-    for (i = 0; i < chosen.length; i++) {
-      titles.push(chosen[i].innerHTML);
+    if (DEBUG) {
+      console.log('drawing');
+    }
+
+    for (i = 0; i < this.chosen.length; i++) {
+      titles.push(this.chosen[i].name);
       dateMaps.push({});
 
-      data = chosen[i]._data[this.periods[this.period]]; // [0] => by year
+      var periodIdx = this.PERIODS[this.period]
+      data = this.chosen[i].data[periodIdx]; 
       for (j = 0; j < data.length; j++) {
         date = data[j][0];
         value = data[j][1];
@@ -93,6 +121,10 @@
       table.push(row);
     }
 
+    this.options.colors = this.chosen.map(function (line) {
+      return line.colour;
+    });
+
     data = google.visualization.arrayToDataTable(table);
     this.options.vAxis.logScale = this.scale === 'logarithmic';
 
@@ -100,6 +132,40 @@
     chart.draw(data, this.options);
   };
 
+  Chart.prototype.add = function (data, id, name) {
+    if (DEBUG) {
+      console.log(data);
+    }
+    this.chosen.push({ 
+      colour: this.colours.pop() || '#000000',
+      name: name,
+      id: id,
+      data: data
+    });
+    this.draw();
+  }
+
+  Chart.prototype.remove = function (id) {
+    for (var i = 0; i < this.chosen.length; i++) {
+      console.log(this.COLOURS);
+      if (this.chosen[i].id === id) {
+        var colour = this.chosen[i].colour;
+        var COLOURS = this.COLOURS;
+        if (colour != '#000000') {
+          this.colours.push(colour);
+          this.colours.sort(function (c1, c2) {
+            return COLOURS.indexOf(c1) - COLOURS.indexOf(c2);
+          });
+        }
+        this.chosen.splice(i, 1);
+        this.draw();
+        return;
+      }
+    }
+    if (DEBUG) {
+      console.log('remove from chart failed due to invalid id ' + id);
+    }
+  }
   // init cretes the default view of the chart before the
   // user selects data to plot
   Chart.prototype.init = function(column) {
@@ -127,7 +193,7 @@
   };
 
   Chart.prototype.setPeriod = function (period) {
-    if (DEBUG && !(period in this.periods)) {
+    if (DEBUG && !(period in this.PERIODS)) {
       console.log('error: invalid time period ' + period);
     }
     this.period = period;
@@ -142,7 +208,6 @@
      create the list and the elements from the data and handle 
      the styling.
   */
-
   function styleElem(li) {
     li.style.padding = "5px";
     li.style.borderBottom = 'dotted gray 1px';
@@ -178,7 +243,7 @@
     return ul;
   }
 
-  /* when a cdid is plotted, a dom element to represent it is 
+  /* when a cdid is plotted, a dom element to represent it's 
      created and inserted into a list; this handles the user
      removing the cdid from the plot.
   */
@@ -188,11 +253,14 @@
       styleElem(node._node);
 
       $(node).fadeOut(1000).delay(1000).remove();
-      chart.draw();
+      chart.remove(node.id);
     };
   }
 
-
+  /* Handle the user requesting a list element be plotted.
+     Must be attached to each element of the list of cdids 
+     as a click handler. Uses the REST api to retrieve the data.
+  */
   function liClickHandler(node) {
     return function () {
       node.style.background = "#FFCC88";
@@ -208,16 +276,24 @@
 
       var response = $.get('/fetchcolumn/' + node.id, function () {
         var data = JSON.parse(response.responseText);
-        selectedNode._data = data;
-        chart.draw();
+        chart.add(data, node.id, node.innerHTML);
       });
     };
   }
   
-
+  /* take the tokens from an input box and send them to the server
+     via websockets. Needs to be registered as a handler for input
+     events on the input box.
+  */
   function sendTokens(event) {
     var input = document.getElementById("token_input"),
         message = false;
+    if (DEBUG) {
+      console.log('sending tokens');
+    }
+    // numsent is used by the server as a kind of response header.
+    // If we receive a response with an out-of-date numsent it can
+    // be discarded.
     if (!this.numsent) {
       this.numsent = 0;
     }
@@ -230,8 +306,28 @@
     }
 
     asock.send(JSON.stringify([++this.numsent, message]));
+
   }
 
+  /* Callback for websocket responses from the server. 
+
+     The response consists of a cdid, an id for the associated 
+     data, the name of the cdid and a list of the names of the
+     datasets where the data is found. A cdid may occur more than
+     once if it has multiple distinct data associated with it.
+
+     The data is rendered into an ordered list by the functions above.
+
+     Unfortunately there responses may have thousands of elements and 
+     queries may take significant fractions of a second to process, so
+     to achieve responsive live-search the strategy is:
+
+     1. Only transfer ~50 elements at a time from the server.
+     2. Generate the dom objects incrementally as the elements arrive.
+     3. But only render the first 50 elements until all elements have arrived.
+     4. If a new query is started before the old one is finished, the old 
+        query is abandoned serverside, as is all unrendered clientside data.
+  */
   var receiveCDIDs = (function () {
     var lastIdent = -1, 
         buffer = [];
@@ -272,14 +368,8 @@
   }());
 
 
-  var asock = new WebSocket("ws://127.0.0.1:8000/tokenmatcher");
-
-  asock.onopen = function (event) {
-    var input = $("#token_input");
-    input.on('input', sendTokens);
-    asock.onmessage = receiveCDIDs;
-  };
-
+  /* Setup and callbacks for the period and scale UI widgets 
+  */
   function uiSelector(list, elem, setter) {
     return function () {
       elements = $(list).children();
@@ -299,7 +389,23 @@
     }
 
   }
+
+
+  var asock;
   $(document).ready( function () {
+    asock = new WebSocket("ws://127.0.0.1:8000/tokenmatcher");
+
+    asock.onopen = function (event) {
+
+      var input = document.getElementById("token_input");
+      if (DEBUG) {
+        console.log('opened websocket');
+        console.log(input);
+      }
+      input.oninput = sendTokens;
+      asock.onmessage = receiveCDIDs;
+    };
+
     initUiList($('#ui_time_period'), function (z) { chart.setPeriod(z); } );
     initUiList($('#ui_chart_scale'), function (z) { chart.setScale(z); });
   });
